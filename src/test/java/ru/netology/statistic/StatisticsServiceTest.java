21e4014c80df80e4f5cfa1d85efa7c75510eec93
Murat Oralbaev <successmanonly@gmail.com>
2023-04-11 20:23:51 +0300

    StatisicsServiceTest.java modified

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMaxIfCurrent() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {13, 12, 28, 4, 5, 0, 8, 6, 190, 11, 12};
+        long expected = 190;
+
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+
+
+    }
 }
